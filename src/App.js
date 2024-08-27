40aa45ea97544283470a10c511adfece8fc36bde
unknown <sowndharya1704@gmail.com>
2024-08-27 21:59:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Chess Board</h1>
+      <table>
+        <tr>
+          <td className='white' align='center'>♜</td>
+          <td className='black' align='center'>♞</td>
+          <td className='white' align='center'>♝</td>
+          <td className='black' align='center'>♛</td>
+          <td className='white' align='center'>♚</td>
+          <td className='black' align='center'>♝</td>
+          <td className='white' align='center'>♞</td>
+          <td className='black' align='center'>♜</td>
+        </tr>
+        <tr>
+        <td className='black' align='center'>♟</td>
+          <td className='white' align='center'>♟</td>
+          <td className='black' align='center'>♟</td>
+          <td className='white' align='center'>♟</td>
+          <td className='black' align='center'>♟</td>
+          <td className='white' align='center'>♟</td>
+          <td className='black' align='center'>♟</td>
+          <td className='white' align='center'>♟</td>
+        </tr>
+        <tr>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+        </tr>
+        <tr>
+        <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+        </tr>
+        <tr>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+        </tr>
+        <tr>
+        <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+          <td className='black' align='center'></td>
+          <td className='white' align='center'></td>
+        </tr>
+        <tr>
+          <td className='white' align='center'>♙</td>
+          <td className='black' align='center'>♙</td>
+          <td className='white' align='center'>♙</td>
+          <td className='black' align='center'>♙</td>
+          <td className='white' align='center'>♙</td>
+          <td className='black' align='center'>♙</td>
+          <td className='white' align='center'>♙</td>
+          <td className='black' align='center'>♙</td>
+        </tr>
+        <tr>
+          <td className='black' align='center'>♖</td>
+          <td className='white' align='center'>♘</td>
+          <td className='black' align='center'>♗</td>
+          <td className='white' align='center'>♕</td>
+          <td className='black' align='center'>♔</td>
+          <td className='white' align='center'>♗</td>
+          <td className='black' align='center'>♘</td>
+          <td className='white' align='center'>♖</td>
+        </tr>
+      </table>
     </div>
   );
 }
